5277822cf8df6c4a54a4763b40356872085cbe85
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2025-03-27 12:31:26 +0700

    fix ckeditor

--- src/resources/views/crud/fields/ckeditor.blade.php
+++ src/resources/views/crud/fields/ckeditor.blade.php
@@ -3,6 +3,26 @@
 $field['extra_plugins'] = isset($field['extra_plugins']) ? implode(',', $field['extra_plugins']) : "embed,widget";
 
 $defaultOptions = [
+"language" => "en",
+"toolbar" => [
+    "undo",
+    "redo",
+    "|",
+    "heading",
+    "outdent",
+    "indent",
+    "|",
+    "bold",
+    "italic",
+    "|",
+    "blockQuote",
+    "insertTable",
+    "bulletedList",
+    "numberedList",
+    "|",
+    "link",
+    "mediaEmbed"
+],
 "filebrowserBrowseUrl" => backpack_url('elfinder/ckeditor'),
 "extraPlugins" => $field['extra_plugins'],
 "embed_provider" => "//ckeditor.iframe.ly/api/oembed?url={url}&callback={callback}",
@@ -12,15 +32,25 @@ $field['options'] = array_merge($defaultOptions, $field['options'] ?? []);
 @endphp
 
 @include('crud::fields.inc.wrapper_start')
+<div
+	class="form-group col-sm-12 mb-3 required"
+	element="div"
+	bp-field-wrapper="true"
+	bp-field-name="content"
+	bp-field-type="ckeditor"
+	bp-section="crud-field"
+>
 <label>{!! $field['label'] !!}</label>
 @include('crud::fields.inc.translatable_icon')
 <textarea
     name="{{ $field['name'] }}"
     data-init-function="bpFieldInitCKEditorElement"
     data-options="{{ trim(json_encode($field['options'])) }}"
+    data-elfinder="false"
+    bp-field-main-input
     @include('crud::fields.inc.attributes', ['default_class' => 'form-control'])
 >{{ old(square_brackets_to_dots($field['name'])) ?? $field['value'] ?? $field['default'] ?? '' }}</textarea>
-
+</div>
 {{-- HINT --}}
 @if (isset($field['hint']))
 <p class="help-block">{!! $field['hint'] !!}</p>
@@ -38,31 +68,101 @@ $crud->markFieldTypeAsLoaded($field);
 
 {{-- FIELD JS - will be loaded in the after_scripts section --}}
 @push('crud_fields_scripts')
-<script src="{{ asset('packages/ckeditor/ckeditor.js') }}"></script>
-<script src="{{ asset('packages/ckeditor/adapters/jquery.js') }}"></script>
-<script>
-    function bpFieldInitCKEditorElement(element) {
+@basset('https://cdn.ckeditor.com/ckeditor5/37.1.0/classic/ckeditor.js')
+@bassetBlock('ProFieldsCkeditor.js')
+<script type="text/javascript">
+// global variable to store elfinder element while waiting for colorbox to close
+var elfinderTarget = false;
 
-
-        //when removing ckeditor field from page html the instance is not properly deleted.
-        //this event is triggered in repeatable on deletion so this field can intercept it
-        //and properly delete the instances so it don't throw errors of unexistent elements in page that has initialized ck instances.
-        element.on('backpack_field.deleted', function(e) {
-            $ck_instance_name = element.siblings("[id^='cke_editor']").attr('id');
-
-            //if the instance name starts with cke_ it was an auto-generated name from ckeditor
-            //that happens because in repeatable we stripe the field names used by ckeditor, so it renders a random name
-            //that starts with cke_
-            if($ck_instance_name.startsWith('cke_')) {
-                $ck_instance_name = $ck_instance_name.substr(4);
-            }
-            //we fully destroy the instance when element is deleted from the page.
-            CKEDITOR.instances[$ck_instance_name].destroy(true);
-        });
-        // trigger a new CKEditor
-        element.ckeditor(element.data('options'));
+// if processSelectedMultipleFiles is not defined, define it
+if (typeof window.processSelectedMultipleFiles !== 'function') {
+    function processSelectedMultipleFiles(files, input) {
+        elfinderTarget.trigger('createInputsForItemsSelectedWithElfinder', [files]);
+        elfinderTarget = false;
     }
+}
+
+async function bpFieldInitCKEditorElement(element) {
+    let hasElfinder = element.data('elfinder');
+    let elfinderTriggerUrl = element.data('elfinder-trigger-url');
+    // To create CKEditor 5 classic editor
+    let ckeditorInstance = await ClassicEditor.create(element[0], element.data('options')).catch(error => {
+        console.error( error );
+    });
+
+    if (!ckeditorInstance) return;
+    if(hasElfinder) {
+        let ckf = ckeditorInstance.commands.get('ckfinder');
+        if (ckf) {
+            // Take over ckfinder execute()
+            ckf.execute = () => {
+                window.ckeditorInstance = ckeditorInstance;
+                window.elfinderOptions = element.data('elfinder-options') ?? {};
+
+                // remember which element the elFinder was triggered by
+                elfinderTarget = element;
+                
+                // trigger the reveal modal with elfinder inside
+                $.colorbox({
+                    href: elfinderTriggerUrl,
+                    fastIframe: false,
+                    iframe: true,
+                    width: '80%',
+                    height: '80%',
+                    onClosed: function () {
+                        elfinderTarget = false;
+                        window.ckeditorInstance = null;
+                        window.elfinderOptions = {};
+                    },
+                });
+            };
+        }
+
+        element.on('createInputsForItemsSelectedWithElfinder', function (e, files) {
+            let imgs = [];
+            
+            $.each(files, function(i, f) {
+                if (f && f.mime.match(/^image\//i)) {
+                    imgs.push(f.url);
+                } else {
+                    ckeditorInstance.execute('link', f.url);
+                }
+            });
+            if (imgs.length) {
+                const ntf = ckeditorInstance.plugins.get('Notification');
+                const i18 = ckeditorInstance.locale.t;
+                const imgCmd = ckeditorInstance.commands.get('imageUpload');
+                if (!imgCmd.isEnabled) {
+                    ntf.showWarning(i18('Could not insert image at the current position.'), {
+                        title: i18('Inserting image failed'),
+                        namespace: 'ckfinder'
+                    });
+                    return;
+                }
+                ckeditorInstance.execute('imageInsert', { source: imgs });
+            }
+        });
+    }
+
+    element.on('CrudField:delete', function (e) {
+        ckeditorInstance.destroy();
+    });
+
+    // trigger the change event on textarea when ckeditor changes
+    ckeditorInstance.editing.view.document.on('layoutChanged', function (e) {
+        element.trigger('change');
+    });
+
+    element.on('CrudField:disable', function (e) {
+        ckeditorInstance.enableReadOnlyMode('CrudField');
+    });
+
+    element.on('CrudField:enable', function (e) {
+        ckeditorInstance.disableReadOnlyMode('CrudField');
+    });
+}
 </script>
+@endBassetBlock
 @endpush
 
 @endif
